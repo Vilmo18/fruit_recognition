f26e948951e482b8e0d4c8ae1a602c312ee2bcfa
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2022-11-27 16:44:56 +0100

    port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log('les');
+import { express } from 'express';
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(1000, () => {
+  console.log(`Server started on port`);
+});
